95b773630caafb4e53dc488eba0fe9d7993957ac
Ramiro Gómez <ramiro.gj.97@gmail.com>
2024-12-03 22:58:31 -0300

    session router

--- src/middlewares/verifyUser.mis.js
+++ src/middlewares/verifyUser.mis.js
@@ -2,7 +2,7 @@ import { Users } from "../data/models/user.model.js";
 export const verifyUser = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ email });
+    const user = await Users.findOne({ email });
     if (!user) {
       return res.status(401).json({ message: "User not found" });
     }
